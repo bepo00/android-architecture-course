0d73872ec056e1e890ab2b4c44996ae1fe10a8e7
Josip Komparić <komparic.josip@gmail.com>
2019-01-16 21:00:21 +0100

    ViewHolder in QuestionListAdapter

--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListAdapter.kt
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListAdapter.kt
@@ -17,18 +17,22 @@ class QuestionsListAdapter(context: Context,
         fun onQuestionClicked(question: Question)
     }
 
+    private data class ViewHolder(val mTitle: TextView)
+
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         var convertView = view
         if (convertView == null) {
             convertView = LayoutInflater.from(parent.context)
                     .inflate(R.layout.layout_question_list_item, parent, false)
+            val viewHolder = ViewHolder(convertView!!.findViewById(R.id.txt_title))
+            convertView.tag = viewHolder
         }
 
         val question = getItem(position)
 
         // bind the data to views
-        val txtTitle = convertView!!.findViewById<TextView>(R.id.txt_title)
-        txtTitle.text = question!!.title
+        val viewHolder = convertView.tag as ViewHolder
+        viewHolder.mTitle.text = question!!.title
 
         // set listener
         convertView.setOnClickListener { onQuestionClicked(question) }
