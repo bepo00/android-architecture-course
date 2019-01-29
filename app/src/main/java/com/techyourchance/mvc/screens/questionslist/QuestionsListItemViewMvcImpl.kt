ed4d3b394917d8c011440803706ac977a380309c
Josip Komparić <komparic.josip@gmail.com>
2019-01-29 09:02:12 +0100

    BaseObservableViewMvc extraction

--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListItemViewMvcImpl.kt
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListItemViewMvcImpl.kt
@@ -5,27 +5,19 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.techyourchance.mvc.R
 import com.techyourchance.mvc.questions.Question
+import com.techyourchance.mvc.screens.common.BaseObservableViewMvc
 import com.techyourchance.mvc.screens.common.BaseViewMvc
 
-class QuestionsListItemViewMvcImpl(inflater: LayoutInflater, parent: ViewGroup) : BaseViewMvc(), QuestionsListItemViewMvc {
+class QuestionsListItemViewMvcImpl(inflater: LayoutInflater, parent: ViewGroup) : BaseObservableViewMvc<QuestionsListItemViewMvc.Listener>(), QuestionsListItemViewMvc {
     private val txtTitle = findViewById<TextView>(R.id.txt_title)
-    private val listeners = arrayListOf<QuestionsListItemViewMvc.Listener>()
     private lateinit var mQuestion: Question
     init {
         setRootView(inflater.inflate(R.layout.layout_question_list_item, parent, false))
-        getRootView().setOnClickListener { listeners.forEach { listener -> listener.onQuestionClicked(mQuestion)} }
+        getRootView().setOnClickListener { getListeners().forEach { listener -> listener.onQuestionClicked(mQuestion)} }
     }
 
     override fun bindQuestion(question: Question) {
         mQuestion = question
         txtTitle.text = question.title
-    }
-
-    override fun registerListener(listener: QuestionsListItemViewMvc.Listener) {
-        listeners.add(listener) //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun unregisterListener(listener: QuestionsListItemViewMvc.Listener) {
-        listeners.remove(listener)
     }
 }
--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvcImpl.kt
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvcImpl.kt
@@ -6,12 +6,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.techyourchance.mvc.R
 import com.techyourchance.mvc.questions.Question
+import com.techyourchance.mvc.screens.common.BaseObservableViewMvc
 import com.techyourchance.mvc.screens.common.BaseViewMvc
 
-class QuestionsListViewMvcImpl(layoutInflater: LayoutInflater, parent: ViewGroup?) : BaseViewMvc(),QuestionsListAdapter.OnQuestionClickListener, QuestionsListViewMvc {
+class QuestionsListViewMvcImpl(layoutInflater: LayoutInflater, parent: ViewGroup?) : BaseObservableViewMvc<QuestionsListViewMvc.Listener>(),QuestionsListAdapter.OnQuestionClickListener, QuestionsListViewMvc {
     private val mRecyclerView: RecyclerView
     private val mRecyclerViewAdapter: QuestionsListAdapter
-    private val mListeners = arrayListOf<QuestionsListViewMvc.Listener>()
 
     init {
         setRootView(layoutInflater.inflate(R.layout.layout_questions_list, parent, false))
@@ -25,15 +25,7 @@ class QuestionsListViewMvcImpl(layoutInflater: LayoutInflater, parent: ViewGroup
         mRecyclerViewAdapter.bindQuestions(questions)
     }
 
-    override fun registerListener(listener: QuestionsListViewMvc.Listener) {
-        mListeners.add(listener)
-    }
-
-    override fun unregisterListener(listener: QuestionsListViewMvc.Listener) {
-        mListeners.remove(listener)
-    }
-
     override fun onQuestionClicked(question: Question) {
-        mListeners.forEach { it.onQuestionClicked(question) }
+        getListeners().forEach { it.onQuestionClicked(question) }
     }
 }
